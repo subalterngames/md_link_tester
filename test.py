53d661cb52b48887a33f2634ed8e73ecb8b85a51
Seth Alter <alters@mit.edu>
2021-09-09 11:47:56 -0400

    removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from md_link_tester import MdLinkTester
-
-bad_links = MdLinkTester.test_directory(path="doc", ignore_files=["another_page.md"])
-for f in bad_links:
-    print(f)
-    for link in bad_links[f]:
-        print("\t", link)
